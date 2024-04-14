680e9e47dc6c60f9b3d3232dd9f71d67d54b3d82
nmelgiri <melgirinik@gmail.com>
2024-04-13 23:54:09 -0400

    Finalized general formula for converting desired accelerations to thrust

--- agents/pid_manual.py
+++ agents/pid_manual.py
@@ -1,2 +1,156 @@
-from mujoco import MjData, MjModel
+from typing import List
 
+import numpy as np
+from mujoco import MjData, MjModel, mj_step, mj_name2id, mjtSensor, mjtObj, viewer, mj_id2name, mj_fullM
+from environments.mujoco_xmls import make_model
+
+
+class PID:
+    current_error = 0
+    previous_error = 0
+    integral_error = 0
+    derivative_error = 0
+
+    def __init__(self, kp, ki, kd, max_integral_error):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.max_integral_error = max_integral_error
+
+    def update(self, error):
+        self.current_error = error
+        self.integral_error += error
+        self.integral_error = np.clip(self.integral_error, -self.max_integral_error, self.max_integral_error)
+        self.derivative_error = error - self.previous_error
+        self.previous_error = error
+        return self.kp * self.current_error + self.ki * self.integral_error + self.kd * self.derivative_error
+
+    def reset(self):
+        self.current_error = 0
+        self.previous_error = 0
+        self.integral_error = 0
+        self.derivative_error = 0
+
+
+class Propeller:
+    def __init__(self, x, y, f):
+        self.x = x
+        self.y = y
+        self.f = f
+
+
+class ManualDrone:
+    # region Initialization
+    def __init__(self, roll_pid: PID, pitch_pid: PID, yaw_pid: PID,
+                 max_roll: float, max_pitch: float, max_yaw: float):
+        # region Initializing the Attributes
+        index = 0
+        self.model = make_model(1, [])
+        self.data = MjData(self.model)
+        drone_name = f"drone_{index}"
+        self.drone_id = mj_name2id(self.model, mjtObj.mjOBJ_BODY, drone_name)
+        self.propeller_body_ids = []
+        self.propeller_masses = []
+        self.motor_ids = []
+        self.max_thrust = 0
+        self.max_torque = 0
+        self.drone_mass = 0
+        self.I = np.zeros((3, 3))
+        self.propellers = []
+        self.A = np.zeros((6, len(self.propellers)))
+        self.A_inv = np.zeros((len(self.propellers), 6))
+        self.yaw_pid = yaw_pid
+        self.pitch_pid = pitch_pid
+        self.roll_pid = roll_pid
+        self.max_roll = max_roll
+        self.max_pitch = max_pitch
+        self.max_yaw = max_yaw
+        # endregion
+
+        # region Calling the Initialization Functions
+        self.initialize_propellers(index)
+        self.initialize_motors(index)
+        self.initialize_mass_and_inertia()
+        self.initialize_thrust_matrix()
+        # endregion
+
+    def initialize_propellers(self, index):
+        propeller_body_names = [
+            f"cw_front_left_{index}",
+            f"ccw_back_left_{index}",
+            f"cw_back_right_{index}"
+            f"ccw_front_right_{index}",
+        ]
+        self.propeller_body_ids = [mj_name2id(self.model, mjtObj.mjOBJ_BODY, name) for name in propeller_body_names]
+        self.propeller_masses = [self.model.body_mass[prop_id] for prop_id in self.propeller_body_ids]
+
+    def initialize_motors(self, index):
+        motor_names = [f"front_right_{index}", f"front_left_{index}", f"back_left_{index}", f"back_right_{index}"]
+        self.motor_ids = [mj_name2id(self.model, mjtObj.mjOBJ_ACTUATOR, name) for name in motor_names]
+        self.max_thrust = self.model.actuator_gear[self.motor_ids[0]][2]
+        self.max_torque = self.model.actuator_gear[self.motor_ids[0]][5]
+
+    def initialize_mass_and_inertia(self):
+        self.drone_mass = self.model.body_mass[self.drone_id] + sum(self.propeller_masses)
+        ixx, iyy, izz, ixy, ixz, iyz = self.data.cinert[self.drone_id][:6]
+        self.I = np.array([
+            [ixx, ixy, ixz],
+            [ixy, iyy, iyz],
+            [ixz, iyz, izz]
+        ]).copy()  # Copy is necessary to avoid the changes in the original data
+
+    def initialize_thrust_matrix(self):
+        f = np.abs(self.max_torque / self.max_thrust)
+        self.propellers = [
+            Propeller(
+                *self.get_relative_position(self.drone_id, pid)[:2],  # Unpack the first two elements from the tuple
+                f if "ccw" in mj_id2name(self.model, mjtObj.mjOBJ_BODY, pid) else -f
+            ) for pid in self.propeller_body_ids
+        ]
+
+        self.A = np.array([
+            [0 for _ in range(len(self.propellers))],
+            [0 for _ in range(len(self.propellers))],
+            [1 for _ in range(len(self.propellers))],
+            [prop.y for prop in self.propellers],
+            [prop.x for prop in self.propellers],
+            [prop.f for prop in self.propellers]
+        ])
+        self.A_inv = np.linalg.pinv(self.A)
+    # endregion
+
+    # region Roll, Pitch, Yaw, and Relative Position
+    @property
+    def roll(self):
+        quaternion = self.data.xquat[self.drone_id]
+        return np.arctan2(2 * (quaternion[0] * quaternion[1] + quaternion[2] * quaternion[3]),
+                          1 - 2 * (quaternion[1] ** 2 + quaternion[2] ** 2))
+
+    @property
+    def pitch(self):
+        quaternion = self.data.xquat[self.drone_id]
+        return np.arcsin(2 * (quaternion[0] * quaternion[2] - quaternion[3] * quaternion[1]))
+
+    @property
+    def yaw(self):
+        quaternion = self.data.xquat[self.drone_id]
+        return np.arctan2(2 * (quaternion[0] * quaternion[3] + quaternion[1] * quaternion[2]),
+                          1 - 2 * (quaternion[2] ** 2 + quaternion[3] ** 2))
+
+    def get_relative_position(self, parent_body_id: int, child_body_id: int) -> np.ndarray:
+        parent_pos = self.data.xpos[parent_body_id]
+        child_pos = self.data.xpos[child_body_id]
+
+        parent_rotation_matrix = self.data.xmat[parent_body_id].reshape((3, 3))
+        global_relative_position = child_pos - parent_pos
+
+        return parent_rotation_matrix.T @ global_relative_position
+    # endregion
+
+    def thrust(self, global_linear_acceleration: np.ndarray,
+               global_angular_acceleration: np.ndarray) -> np.ndarray:
+        rotation_matrix = self.data.xmat[self.drone_id].reshape((3, 3))
+        return np.clip(np.divide(self.A_inv @ np.concatenate([
+            rotation_matrix @ (self.drone_mass * global_linear_acceleration),
+            rotation_matrix @ (self.I @ global_angular_acceleration)
+        ]), self.max_thrust), 0, 1)
